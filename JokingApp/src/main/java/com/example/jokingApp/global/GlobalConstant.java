092c3c56dd03b583e4f4cd65654086aff0ad03bf
Brienty <568620657@qq.com>
2016-03-21 21:41:04 +0800

    基本布局搭建

---
+++ JokingApp/src/main/java/com/example/jokingApp/global/GlobalConstant.java
@@ -0,0 +1,8 @@
+package com.example.jokingApp.global;
+
+/**
+ * Created by idea-pc on 2016/3/21.
+ */
+public class GlobalConstant {
+    public  static  final String SERVER_URL="http://121.42.200.58/";
+}
